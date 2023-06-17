60708a04bddea92321675daf55f86fd0d3e6ed0e
Akash km <akashmanoj999@gmail.com>
2023-06-17 14:28:36 +0530

    day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
+
+
+
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Header from './units/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+    </div>
   )
 }
 
---
+++ src/units/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Header() {
+  return (
+    <div>
+<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
